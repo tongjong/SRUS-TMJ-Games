16f8761d6a8c03f640d55ccdfa7177425d67e1b5
J155917 <J155917@tafe.wa.edu.au>
2025-03-07 10:42:41 +0800

    feat: implement hash functions in Player class

--- app/player.py
+++ app/player.py
@@ -1,4 +1,11 @@
+import random
+
+
 class Player:
+    random.seed(20)
+    pearson_table = list(range(256))
+    random.shuffle(pearson_table)
+
     def __init__(self, uid: str, name: str):
         self._uid = uid
         self._name = name
@@ -10,6 +17,24 @@ class Player:
     @property
     def name(self):
         return self._name
+
+    @classmethod
+    def hash(cls, key: str) -> int:
+        byte_values = bytes(key, encoding='utf8')
+        _hash = 0
+        for each_byte in byte_values:
+            _hash = cls.pearson_table[_hash ^ each_byte]
+        return _hash
+
+    def _pearson_hash(self, key) -> int:
+        byte_values = bytes(key, encoding='utf8')
+        _hash = 0
+        for each_byte in byte_values:
+            _hash = self.pearson_table[_hash ^ each_byte]
+        return _hash
+
+    def __hash__(self) -> int:
+        return self._pearson_hash(self._uid)
 
     def __str__(self):
         return f'Unique Id: {self.uid}, Player Name: {self.name}'
