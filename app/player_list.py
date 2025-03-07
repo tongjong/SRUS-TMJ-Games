6f6cbde2762745b9942ab17239e6ce31094f6aba
J155917 <J155917@tafe.wa.edu.au>
2025-03-07 10:06:37 +0800

    implement add_at_head method and testcase in PlayerList class

--- app/player_list.py
+++ app/player_list.py
@@ -1,4 +1,21 @@
+from app.player_node import PlayerNode
+
+
 class PlayerList:
     def __init__(self):
         self._head = None
-        self._tail = None
+
+    @property
+    def head(self):
+        return self._head
+
+    def add_at_head(self, player_node: PlayerNode) -> None:
+        if self._is_empty():
+            self._head = player_node
+        else:
+            player_node.next_node = self._head
+            self._head.prev_node = player_node
+            self._head = player_node
+
+    def _is_empty(self) -> bool:
+        return self._head is None
---
+++ test/player_list_test.py
@@ -0,0 +1,43 @@
+import unittest
+from app.player import Player
+from app.player_list import PlayerList
+from app.player_node import PlayerNode
+
+
+class TestPlayerList(unittest.TestCase):
+    def setUp(self):
+        self.player_list = PlayerList()
+
+        self.player_1 = Player("1", "John")
+        self.player_node_1 = PlayerNode(self.player_1)
+
+        self.player_2 = Player("2", "Mary")
+        self.player_node_2 = PlayerNode(self.player_2)
+
+        self.player_3 = Player("3", "Bob")
+        self.player_node_3 = PlayerNode(self.player_3)
+
+    def test_add_at_head_when_player_list_empty(self):
+        self.player_list.add_at_head(self.player_node_1)
+
+        self.assertEqual(self.player_list.head, self.player_node_1)
+
+    def test_add_at_head_when_player_list_has_one_node(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+
+        self.assertEqual(self.player_list.head, self.player_node_2)
+        self.assertEqual(self.player_node_2.next_node, self.player_node_1)
+        self.assertEqual(self.player_node_1.prev_node, self.player_node_2)
+
+    def test_add_at_head_when_player_list_has_more_than_one_node(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+
+        self.assertEqual(self.player_list.head, self.player_node_3)
+        self.assertIsNone(self.player_list.head.prev_node)
+        self.assertEqual(self.player_node_2.next_node, self.player_node_1)
+        self.assertEqual(self.player_node_2.prev_node, self.player_node_3)
+        self.assertEqual(self.player_node_1.prev_node, self.player_node_2)
+        self.assertEqual(self.player_node_3.next_node, self.player_node_2)
