f45616db705a309b086b00f5097ace92c5010abd
J155917 <J155917@tafe.wa.edu.au>
2025-04-10 17:08:51 +0800

    wip(player_tree): Create instance variables and initialiser method for PlayerBST and PlayerBNode classes

---
+++ app/player_bnode.py
@@ -0,0 +1,28 @@
+from app.player import Player
+
+
+class PlayerBNode:
+    def __init__(self, player: Player):
+        self._player = player
+        self._left = None
+        self._right = None
+
+    @property
+    def player(self):
+        return self._player
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self.right
+
+    @left.setter
+    def left(self, player: Player):
+        self._left = player
+
+    @right.setter
+    def right(self, player: Player):
+        self._right = player
---
+++ app/player_bst.py
@@ -0,0 +1,7 @@
+class PlayerBST:
+    def __init__(self):
+        self._root = None
+
+    @property
+    def root(self):
+        return self._root
